b9c4260f8c0d618f46aeaaf6681ccd5685614a17
xdirskswtrej <103025501+xdirskswtrej@users.noreply.github.com>
2023-05-10 08:57:50 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+UUID=${UUID:-'e3e6401d-c86d-4b98-b08d-f78c0468545b'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/meee'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/leee'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
